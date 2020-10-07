519f8f3f34a77b5cccf0cfeab8e60805e9018971
Kamil Krzyszowski <krzyszowskikamil@gmail.com>
2020-10-07 20:43:01 +0200

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LocalHost
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var sampleClass = new Sample(10);
+            DoSomething(sampleClass);
+
+            Console.WriteLine(sampleClass?.Value);
+
+            void DoSomething(Sample sampleClasss)
+            {
+                sampleClass = null;
+            }
+        }
+    }
+
+    public class Sample
+    {
+        public int Value { get; set; }
+        public Sample(int value) { Value = value; }
+    }
+}
+
