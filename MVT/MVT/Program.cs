8d2d0ebb9291747bf55b09f0530d5883da9b502f
Kamil Krzyszowski <krzyszowskikamil@gmail.com>
2020-10-07 21:51:00 +0200

    sd

--- MVT/MVT/Program.cs
+++ MVT/MVT/Program.cs
@@ -6,7 +6,7 @@ namespace MVT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");//trtrtdvzx
+            Console.WriteLine("Hello World!");//trtr
         }
     }
 }
